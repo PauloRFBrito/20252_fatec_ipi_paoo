49fff005c1352dbaee204f614c50382e74f09bad
Paulo Brito <pricardof27@gmail.com>
2025-09-03 18:57:18 -0300

    learn: Objetos em JavaScript

--- js/intro/script.js
+++ js/intro/script.js
@@ -1,93 +1,122 @@
-//Aula 3
-//CLOSURES
-//-------------------------------------------------------------------
-// closure é uma função que se "lembra" do ambiente — ou escopo — em que foi criada.** Isso significa que ela carrega consigo, como em uma "mochila", todas as variáveis que estavam ao seu alcance no momento de sua criação, mesmo que ela seja executada em um lugar e momento completamente diferentes.
-//-------------------------------------------------------------------
+// //Aula 3
+// //CLOSURES
+// //-------------------------------------------------------------------
+// // closure é uma função que se "lembra" do ambiente — ou escopo — em que foi criada.** Isso significa que ela carrega consigo, como em uma "mochila", todas as variáveis que estavam ao seu alcance no momento de sua criação, mesmo que ela seja executada em um lugar e momento completamente diferentes.
+// //-------------------------------------------------------------------
 
-let umaFuncao = function() {
-    console.log('Fui armazenada em uma variável')
-}
-umaFuncao()
+// let umaFuncao = function() {
+//     console.log('Fui armazenada em uma variável')
+// }
+// umaFuncao()
 
-function f(funcao) { // Função de mais alta ordem!
-    funcao()
-}
-f(umaFuncao)
+// function f(funcao) { // Função de mais alta ordem!
+//     funcao()
+// }
+// f(umaFuncao)
 
-//Função de alta ordem -> Recebe outra função como parâmetro ou
-//Retorna uma função 
-//-------------------------------------------------------------------
-// Se declarado: f(umaFuncao()), não funciona! 
-// Os () internos são resolvidos primeiro. 
-// No exemplo acima esta sendo passado o resultado da funcão "umaFuncao". 
-// Ela devolve undefienied. 
-// Se não tem return definido, ele está implicito.
-//-------------------------------------------------------------------
+// //Função de alta ordem -> Recebe outra função como parâmetro ou
+// //Retorna uma função 
+// //-------------------------------------------------------------------
+// // Se declarado: f(umaFuncao()), não funciona! 
+// // Os () internos são resolvidos primeiro. 
+// // No exemplo acima esta sendo passado o resultado da funcão "umaFuncao". 
+// // Ela devolve undefienied. 
+// // Se não tem return definido, ele está implicito.
+// //-------------------------------------------------------------------
   
-function g(){
-    function outraFuncao(){
-        console.log('Fui criada por g')
-    }
-    return outraFuncao() //Executa e retorna o resultado
+// function g(){
+//     function outraFuncao(){
+//         console.log('Fui criada por g')
+//     }
+//     return outraFuncao() //Executa e retorna o resultado
+// }
+// // g()
+
+// function g(){
+//     return function() {
+//         console.log("Sou de alta ordem porque retorno outra função!")
+//     }
+// }
+// const gResult = g()
+// // gResult()
+
+// f(function() {
+//     console.log("Sou uma function passada para f")
+// }) //A impressão ocorre em f(umaFuncao).
+
+// f(() => console.log("Sou uma arrow passada para f"))
+
+// //-------------------------------------------------------------------
+// // Exemplos de Funções que recebem outras funções e as executam
+
+// // gResult() -> Executa função g()
+
+// // g()() -> g() executa g e retorna uma função; g()() executa a função retornada
+
+// // f(g) -> passa a função g() como parâmetro para f() 
+
+// // f(g()) -> executa g() e o retorno de g() é passado para f() 
+
+// // f(g()()) -> executa g() que deve retornar uma função; o () extra executa  afunção retornada; o resultado final vai como argumento para f().
+
+// // f(g())() -> executa g(), amda o resultado para f(); f(g())() precisa retornar uma função que é executado no último ()
+// //-------------------------------------------------------------------
+
+// function f(){
+//     let nome = 'João'
+//     function g(){
+//         console.log(nome)
+//     }
+//     g()
+// }
+// f()
+
+// function ola(){
+//     let nome = 'JOÃO'
+//     return function (){
+//         console.log(`Ola, ${nome}`)
+//     }
+// }
+// let olaResult = ola() // Function ola terminou aqui.
+// olaResult()
+
+// function saudacoesFactory(saudacao, nome){
+//     return function() { 
+//         console.log(`${saudacao}, ${nome}`)
+//     }
+// }
+// let olaPedro = saudacoesFactory('ola', 'Pedro')
+// let tchauPedro = saudacoesFactory('tchau', 'Pedro')
+// olaPedro()
+// tchauPedro()
+
+// OBJETOS JavaScript
+
+// Anotação {} -> uma pessoa se chama João e tem 17 anos
+// Coleção de pares chave-valores
+
+let pessoa = {
+    nome: 'João', // Um par de chave-valor, é separado por vírgulas.
+    idade: 17
 }
-// g()
-
-function g(){
-    return function() {
-        console.log("Sou de alta ordem porque retorno outra função!")
-    }
-}
-const gResult = g()
-// gResult()
-
-f(function() {
-    console.log("Sou uma function passada para f")
-}) //A impressão ocorre em f(umaFuncao).
-
-f(() => console.log("Sou uma arrow passada para f"))
-
-//-------------------------------------------------------------------
-// Exemplos de Funções que recebem outras funções e as executam
-
-// gResult() -> Executa função g()
-
-// g()() -> g() executa g e retorna uma função; g()() executa a função retornada
-
-// f(g) -> passa a função g() como parâmetro para f() 
-
-// f(g()) -> executa g() e o retorno de g() é passado para f() 
-
-// f(g()()) -> executa g() que deve retornar uma função; o () extra executa  afunção retornada; o resultado final vai como argumento para f().
-
-// f(g())() -> executa g(), amda o resultado para f(); f(g())() precisa retornar uma função que é executado no último ()
-//-------------------------------------------------------------------
-
-function f(){
-    let nome = 'João'
-    function g(){
-        console.log(nome)
-    }
-    g()
-}
-f()
-
-function ola(){
-    let nome = 'JOÃO'
-    return function (){
-        console.log(`Ola, ${nome}`)
-    }
-}
-let olaResult = ola() // Function ola terminou aqui.
-olaResult()
-
-function saudacoesFactory(saudacao, nome){
-    return function() { 
-        console.log(`${saudacao}, ${nome}`)
-    }
-}
-let olaPedro = saudacoesFactory('ola', 'Pedro')
-let tchauPedro = saudacoesFactory('tchau', 'Pedro')
-olaPedro()
-tchauPedro()
+console.log('Me chamo ' + pessoa.nome)
+console.log('Minha idade é ' + pessoa['idade'])
 
   
+// Objeto JavaScript não é sinonimo de obejto JSON(Java Script Object Notation), mas são parecidos.
+
+// uma pessoa de chama Maria, tem 21 anos e mora na rua B, número 121
+
+let pessoa2 = {
+    nome: 'Maria',
+    idade: 21,
+    endereco: {
+        logradouro: 'Rua B',
+        numero: 121
+    }
+}
+console.log('Me chamo ' + pessoa2.nome)
+console.log('tenho ' + pessoa2.idade + ' anos')
+console.log('Resido na ' + pessoa2.endereco.logradouro + '-' 
+                         + pessoa2.endereco.numero)
+
